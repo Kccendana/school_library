fb170d26fe2d88725147a0cd9c6d61743942aac7
kcccendana <kc_telyn@yahoo.com>
2023-10-04 01:27:49 +0900

    Update trimmer_decorator.rb

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,7 +2,6 @@ require_relative 'base_decorator'
 
 class TrimmerDecorator < BaseDecorator
   def correct_name
-    trimmed_name = @nameable.correct_name[0..9]
-    trimmed_name.length == 10 ? trimmed_name : @nameable.correct_name
+    @nameable.correct_name[0..9]
   end
 end
