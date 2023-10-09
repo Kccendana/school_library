06fb8034bd5be79f3bff434077a64a524325e423
kcccendana <kc_telyn@yahoo.com>
2023-10-09 22:11:19 +0900

    Update book.rb

--- book.rb
+++ book.rb
@@ -4,10 +4,6 @@ class Book
   def initialize(title, author)
     @title = title
     @author = author
-    @rentals = []
   end
 
-  def add_rental(date, person)
-    @rentals.push(Rental.new(date, self, person)) unless @rentals.include?(Rental.new(date, self, person))
-  end
 end
